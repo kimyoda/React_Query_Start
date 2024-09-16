63f5893455b4c053268ce3f93b6328480e02cc61
kimyoda <rladygks1210@naver.com>
2024-09-16 18:19:59 +0900

    [fix] .env modify

--- react-query-course/src/App.tsx
+++ react-query-course/src/App.tsx
@@ -1,17 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 import axios from "axios";
+import { useEffect } from "react";
 
 const getData = async () => {
   return axios.get(import.meta.env.VITE_APP_API_URL);
 };
 
 function App() {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isSuccess, isError } = useQuery({
     queryKey: ["posts"],
     queryFn: getData,
     select: (data) => data.data,
   });
+
+  useEffect(() => {
+    if (isSuccess) console.log("Data fetched successfully");
+  }, [isSuccess, data]);
+
+  useEffect(() => {
+    if (isError) console.log("Error fetching data");
+  }, [isError]);
 
   return (
     <>
