7da57da0fe6d12bbef7e787e4bfb789ece07de70
kimyoda <rladygks1210@naver.com>
2024-09-19 01:59:53 +0900

    [feat] useMutation add, mutate add

--- react-query-course/src/App.tsx
+++ react-query-course/src/App.tsx
@@ -1,17 +1,30 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import "./App.css";
 import { usePost } from "./usePostByid";
 import { usePosts } from "./usePosts";
+import axios from "axios";
+import { IPost } from "./post.types";
 
-const isAuth = true;
+// const isAuth = true;
 
 function App() {
-  const { post } = usePost(1);
-  const { data, isLoading } = usePosts(isAuth);
+  // const { post } = usePost(1);
 
   const queryClient = useQueryClient();
 
-  console.log(post);
+  // useMutation
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["add post"],
+    mutationFn: async (newPost: Omit<IPost, "id">) =>
+      axios.post(import.meta.env.VITE_APP_API_URL, newPost),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+  });
+
+  // const { data, isLoading } = usePosts(isAuth);
+
+  // console.log(post);
 
   return (
     <>
@@ -19,17 +32,23 @@ function App() {
 
       <button
         onClick={() => {
-          queryClient.invalidateQueries({ queryKey: ["posts"] });
+          mutate({
+            body: "New body",
+            title: "New title",
+            userId: 1,
+          });
         }}
+        disabled={isPending}
       >
-        Invalidate posts
+        {/* Invalidate posts */}
+        {isPending ? "Loding..." : "Create"}
       </button>
 
-      {isLoading
+      {/* {isLoading
         ? "Loding..."
         : data?.length
         ? data.map((post: any) => <div key={post.id}>{post.title}</div>)
-        : "Not found"}
+        : "Not found"} */}
     </>
   );
 }
